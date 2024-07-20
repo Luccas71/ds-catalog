161d88034aa46110761b2c2e1fc0f16b40425648
Lucas Volpato <lmaciel916@gmail.com>
2024-07-20 12:43:52 -0300

    feat: DTO

--- backend/src/main/java/com/luccas71/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/luccas71/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.luccas71.dscatalog.services;
 
+import com.luccas71.dscatalog.dto.CategoryDTO;
 import com.luccas71.dscatalog.entities.Category;
 import com.luccas71.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 //permite a injeçao de dependencia automatica
 @Service
@@ -14,7 +17,10 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    //garante que a op é uma transação com db e readonly não trava o banco melhorando a performance
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 }
